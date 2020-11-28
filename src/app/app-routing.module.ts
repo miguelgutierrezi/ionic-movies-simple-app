72c1c82d50ff15eb5ce1294e78220291a10668d4
mguti19 <mguti19@bancodebogota.com.co>
2020-11-28 09:27:20 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,41 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {HomePage} from './pages/home/home.page';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-  {
-    path: 'home',
-    component: HomePage,
-    children: [
-      {
-        path: 'movies',
+    {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+    {
+        path: 'home',
+        component: HomePage,
         children: [
-          {
-            path: '',
-            loadChildren: () =>
-                import('./pages/movies-list/movies-list.module').then(m => m.MoviesListPageModule),
-          }
+            {
+                path: 'movies',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () =>
+                            import('./pages/movies-list/movies-list.module').then(m => m.MoviesListPageModule),
+                    }
+                ]
+            },
+            {
+                path: '',
+                redirectTo: '/home/movies',
+                pathMatch: 'full'
+            }
         ]
-      },
-      {
-        path: '',
-        redirectTo: '/home/movies',
-        pathMatch: 'full'
-      }
-    ]
-  },
-  {
-    path: 'welcome',
-    loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomePageModule)
-  },
+    },
+    {
+        path: 'welcome',
+        loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomePageModule)
+    },
 ];
+
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
